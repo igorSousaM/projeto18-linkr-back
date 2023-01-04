cae662d376321e9a88cfb3d91eab316fffcc1539
GiovanneAndrade <vanne.as@hotmail.com>
2023-01-04 13:27:37 -0300

    fix: remove confirmPassword

--- src/models/signup.schema.js
+++ src/models/signup.schema.js
@@ -4,7 +4,6 @@ const postSignupSchema = Joi.object({
   name: Joi.string().required().min(1).trim(),
   email: Joi.string().email().required().trim(),
   password: Joi.string().required().min(1).trim(),
-  confirmPassword: Joi.ref("password"),
   photo: Joi.string().uri(),
 });
 
