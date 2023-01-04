e2935c953838ef746a9c81cca74130257133c775
GiovanneAndrade <vanne.as@hotmail.com>
2023-01-04 11:05:33 -0300

    feat: signin schema

--- src/routers/signup.router.js
+++ src/routers/signup.router.js
@@ -4,7 +4,7 @@ import { postSignupController } from "../controllers/signup.controller.js";
 import { validateSchema } from "../middlewares/shemaValidation.js";
 import { postSigninMiddlewares } from "../middlewares/signin.middlewares.js";
 import { postSignupMiddlewares } from "../middlewares/signup.middlewares.js";
-import postSigninSchema from "../models/signin.router.js";
+import postSigninSchema from "../models/signin.schema.js";
 import { postSignupSchema } from "../models/signup.schema.js";
 
 const router = Router();
