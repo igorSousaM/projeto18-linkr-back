64b5c003d4a0171a0d98e58eac548d4d5da22923
GiovanneAndrade <vanne.as@hotmail.com>
2023-01-04 11:01:58 -0300

    feat: signin router

--- src/routers/signup.router.js
+++ src/routers/signup.router.js
@@ -1,11 +1,25 @@
 import { Router } from "express";
+import { postSigninController } from "../controllers/signin.controller.js";
 import { postSignupController } from "../controllers/signup.controller.js";
- 
+import { validateSchema } from "../middlewares/shemaValidation.js";
+import { postSigninMiddlewares } from "../middlewares/signin.middlewares.js";
+import { postSignupMiddlewares } from "../middlewares/signup.middlewares.js";
+import postSigninSchema from "../models/signin.router.js";
+import { postSignupSchema } from "../models/signup.schema.js";
 
- 
 const router = Router();
 
-router.post("/signup", postSignupController);
-
+router.post(
+  "/signup",
+  validateSchema(postSignupSchema),
+  postSignupMiddlewares,
+  postSignupController
+);
+router.post(
+  "/signin",
+  validateSchema(postSigninSchema),
+  postSigninMiddlewares,
+  postSigninController
+);
 
 export default router;
